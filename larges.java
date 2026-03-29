8e6205020c020ff5991bb7fe20780ba5cdfbe61b
Gunjan Sharma <bhargavgunjan14@gmail.com>
2026-03-29 21:58:18 +0530

    Add files via upload

---
+++ larges.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class larges{
+    public static void main(String[]args){
+        int a=1;
+        int b=10;
+        if(a>b){
+            System.out.println("Largest number is:"+a);
+        }
+            else{
+                System.out.println("Largest number is b:"+b);
+            }
+        }
+    }
